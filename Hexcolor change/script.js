0929f39184032a3f30f0944e47d45d9eb88b23a7
mingyih93 <55941059+mingyih93@users.noreply.github.com>
2019-09-29 20:31:40 +0800

    update

--- Hexcolor change/script.js
+++ Hexcolor change/script.js
@@ -1,12 +1,27 @@
-const colorBtn = document.querySelector('.colorBtn');
-const bodyBcg = document.querySelector('body');
+const hexNumbers = [0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F']
+const hexBtn = document.querySelector('.hexBtn');
+const hex = document.querySelector('.hex');
+const bodyBcg = document.querySelector('body')
 
-const colors = ['yellow','green','green','#3b5998']
+hexBtn.addEventListener('click', getHex);
 
-colorBtn.addEventListener('click',changeColor)
 
-function changeColor (){
-//bodyBcg.style.backgroundColor = colors[2];
-let random = Math.floor(Math.random()*colors.length)
-bodyBcg.style.backgroundColor = colors[random];
+const demo = document.querySelector('.demo');
+ hexCode = ['0','1','2','3','4'];
+demo.innerHTML = hexCode;
+
+//document.write(out);
+//document.getElementById("demo") = hexOutput;
+
+
+function getHex() {
+  let hexCol = '#';
+
+  for (i=0; i<6; i++) {
+  let random = Math.floor(Math.random()*hexNumbers.length);
+  hexCol +=hexNumbers[random];
+  console.log(hexCol);  //hexOutput.push("a");
+    }
+  bodyBcg.style.backgroundColor = hexCol;
+  hex.innerHTML = hexCol;
 }
